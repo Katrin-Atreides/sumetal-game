582a23cf6d43926f311104a3fd814bfcad0c377a
andebugan <andrebugaenko2012@mail.ru>
2025-04-26 19:47:47 +0300

    #1 basic version of camera and movement controller

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public Transform target; // The object to orbit around
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+    public float yMinLimit = -20f;
+    public float yMaxLimit = 80f;
+    
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+    }
+
+    void Update()
+    {
+        x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+        y = ClampAngle(y, yMinLimit, yMaxLimit);
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+        Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
+
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360f) angle += 360f;
+        if (angle > 360f) angle -= 360f;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
--- Assets/Scripts/MetalCube.cs
+++ Assets/Scripts/MetalCube.cs
@@ -7,38 +7,26 @@ using UnityEngine;
 /// </summary>
 public class MetalCube : MonoBehaviour
 {
-    /// <summary>Speed of the cube</summary>
-    public float speed = 1.0f;
+    /// <summary>Rigidbody reference</summary>
+    protected Rigidbody body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
 
     /// <summary>Moves cube in specified direction on 2D plane</summary>
-    public void Move2D(Quaternion direction) 
+    public void Move2D(Quaternion direction, float speed) 
     {
         // get force vector in direction of quaternion
-        Debug.Log(Vector3.forward);
         Vector3 force = direction * Vector3.forward;
-        Debug.Log(force);
         // get only XZ components (2D plane)
         force = new Vector3(force.x, 0f, force.z);
         // normalize vector to 1, then multiply by speed
         if (force.sqrMagnitude > 0.0001f) force.Normalize();
         force = force * speed;
 
-        Debug.Log(force.ToString());
-
         // add force to rigidbody component
-        var body = GetComponent<Rigidbody>();
         body.AddForce(force);
    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }

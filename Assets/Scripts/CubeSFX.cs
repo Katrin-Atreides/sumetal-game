aab52e136d1f61c81d939b2897d95dff556d0775
Katrin-Atreides <2sparkhighscreen@gmail.com>
2025-04-27 06:28:31 +0300

    Changed some stuff in MainScene

--- Assets/Scripts/CubeSFX.cs
+++ Assets/Scripts/CubeSFX.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CubeSFX : MonoBehaviour
 {
+    AudioSource source;
+    float origPitch;
+    public AudioClip collisionSound, bumpingSound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
+        origPitch = source.pitch;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        source.pitch = Random.Range(origPitch - 0.1f, origPitch + 0.1f);
+
+        if (collision.gameObject.tag == "enemy") source.PlayOneShot(collisionSound);
+        else source.PlayOneShot(bumpingSound);
     }
 }
